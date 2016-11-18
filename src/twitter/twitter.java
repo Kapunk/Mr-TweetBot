d1238ad1c41e0d06685047944924d4e3fb37acfb
kepa <kepa@kapunk>
2016-11-18 18:36:49 +0100

    Añadido código de envio de tweet

--- src/twitter/twitter.java
+++ src/twitter/twitter.java
@@ -1,5 +1,12 @@
 package twitter;
 
+import twitter4j.Paging;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class twitter {
 
 	/**
@@ -9,5 +16,20 @@ public class twitter {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public void Tweet(String Mensaje) throws TwitterException{
+	       Twitter twitter;
+	        ConfigurationBuilder cb = new ConfigurationBuilder();
+	        cb.setDebugEnabled(true)
+	                .setOAuthConsumerKey("Consumer Key")
+	                .setOAuthConsumerSecret("Consumer Secret")
+	                .setOAuthAccessToken("Access Token")
+	                .setOAuthAccessTokenSecret("Access Token Secret");
+	        twitter = new TwitterFactory(cb.build()).getInstance();
+	 
+	 
+	        Status tweetEnviado = twitter.updateStatus(Mensaje);      
+	 
+	    }
 
 }
