a5652f2fb32a10e2a64339d208839dbe8ae6fca9
kepa <kepa@kapunk>
2016-11-28 22:48:20 +0100

    Añadida lectura de tweets por usuario

--- src/twitter/twitter.java
+++ src/twitter/twitter.java
@@ -1,8 +1,10 @@
 package twitter;
 
-import twitter4j.Paging;
+import java.util.List;
+
+import credencial.credencial;
+
 import twitter4j.Status;
-import twitter4j.TweetEntity;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -17,17 +19,19 @@ public class twitter {
 	public static void main(String[] args) throws TwitterException {
 		// TODO Auto-generated method stub
 		
-		 enviarTweet("hola");
+		 leerTweetUsuario("");
 
 	}
-	/*
+	
+	/**
 	 * Metodo para el envio de tweets
-	 * 
+	 * @param Mensaje
+	 * @throws TwitterException
 	 */
 	public static void enviarTweet(String Mensaje) throws TwitterException{
 	       Twitter twitter;
 	       
-	       ConfigurationBuilder cb = credencial.credencial.credencialTwitter();
+	       ConfigurationBuilder cb = credencial.credencialTwitter();
 	       
 	        twitter = new TwitterFactory(cb.build()).getInstance();
 	 
@@ -35,5 +39,32 @@ public class twitter {
 	        Status tweetEnviado = twitter.updateStatus(Mensaje);      
 	 
 	    }
+	
+	/**
+	 * Metodo para leer los tweets de un usuario
+	 * @param usuario
+	 */
+	
+	public static void leerTweetUsuario(String usuario) {
+
+        Twitter twitter = new TwitterFactory().getInstance();
+        try {
+            ConfigurationBuilder cb = credencial.credencialTwitter();
+
+            TwitterFactory tf = new TwitterFactory(cb.build());
+            Twitter twitter1 = tf.getInstance();
+            List<Status> statuses;
+            statuses = twitter1.getUserTimeline(usuario);
+            
+            System.out.println("Mostrando los tweets de @" + usuario);
+            for (Status status : statuses) {
+                System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+            }
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to get timeline: " + te.getMessage());
+            System.exit(-1);
+        }
+    }
 
 }
