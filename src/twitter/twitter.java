7e1e0cb3019dc5253161435e08167862c8a25bc4
kepa <kepa@kapunk>
2016-11-28 21:59:53 +0100

    twitter

--- src/twitter/twitter.java
+++ src/twitter/twitter.java
@@ -28,10 +28,10 @@ public class twitter {
 	       Twitter twitter;
 	        ConfigurationBuilder cb = new ConfigurationBuilder();
 	        cb.setDebugEnabled(true)
-	                .setOAuthConsumerKey("LQiWgVFWFyCRr19HAFshJ2GYY")
-	                .setOAuthConsumerSecret("fS727mXfGz1sTguxH9Dz5QD6AOngwfwymvudYILGgJiMSYtOIo")
-	                .setOAuthAccessToken("799569404061188096-s8Xef5aeJeEVGtfMWZmfcMB8Tvx1oJT")
-	                .setOAuthAccessTokenSecret("XFEMPkPYHlhng1sE34dQQpkRL5tPJ7NpyoOvNW3ql36ML");
+	                .setOAuthConsumerKey("")
+	                .setOAuthConsumerSecret("")
+	                .setOAuthAccessToken("")
+	                .setOAuthAccessTokenSecret("");
 	        twitter = new TwitterFactory(cb.build()).getInstance();
 	 
 	 
