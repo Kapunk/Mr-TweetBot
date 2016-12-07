8f5160d5748c26cd1ed791499adc57f1fb21c01a
kapunk <kepa10@gmail.com>
2016-12-07 11:53:18 +0100

    Actualización estructura

---
+++ src/bot/twitter.java
@@ -0,0 +1,14 @@
+
+package bot;
+
+public class twitter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/intercambio/twitter.java
@@ -0,0 +1,10 @@
+package intercambio;
+
+public class twitter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
