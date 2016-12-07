08e3d31d63c3dabc4642cad5621e65bbc34b67c4
chistian95 <kriskorral59@gmail.com>
2016-12-07 20:40:26 +0100

    Arreglar el classpath y poner un ejemplo de uso

--- src/bot/bot.java
+++ src/bot/bot.java
@@ -1,13 +1,39 @@
 package bot;
 
+import java.util.List;
+
+import twitter.twitter;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.User;
+
 public class bot {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			List<User> amigos = twitter.obtenerListaAmigos("Chistian95");
+			if(amigos.size() > 0) {
+				int rnd = (int) (Math.random() * amigos.size());
+				User amigoRandom = amigos.get(rnd);
+				System.out.println("Follower escogido: " + amigoRandom.getScreenName());	
+				
+				Status maxRT = null;
+				List<Status> tweets = twitter.leerTweetUsuario(amigoRandom.getScreenName());
+				for(int i=0; i<tweets.size(); i++) {
+					Status tweet = tweets.get(i);
+					if(maxRT == null || (!tweet.isRetweet() && tweet.getRetweetCount() > maxRT.getRetweetCount())) {
+						maxRT = tweet;
+					}
+				}
+				
+				System.out.println("Tweet con más RT (" + maxRT.getRetweetCount() + "): " + maxRT.getText());
+			}				
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		} 
 	}
 
 }
